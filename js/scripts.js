9a79c23779c693549565bd83cb0b67291629f841
Micheal Brown and Jacey Tupa <micheal@>
2018-10-18 11:13:14 -0700

    Add img to leopard-info

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("#leopard").click(function() {
+    $(".info").hide();
     $("#leopard-info").slideToggle();
   });
   $("#lion").click(function() {
+    $(".info").hide();
     $("#lion-info").slideToggle();
   });
   $("#giraffe").click(function() {
+    $(".info").hide();
     $("#giraffe-info").slideToggle();
   });
 });
