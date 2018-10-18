4cac4d34c7ab274f71523b93ba3d74dfd0594c08
Micheal Brown and Jacey Tupa <jacey@>
2018-10-18 11:01:47 -0700

    initialize neccissary setup

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#leopard").click(function() {
+    $("#leopard-info").slideToggle();
+  });
+  $("#lion").click(function() {
+    $("#lion-info").slideToggle();
+  });
+  $("#giraffe").click(function() {
+    $("#giraffe-info").slideToggle();
+  });
+});
